924267b2c22d36854df091b99a326e41d6a80215
MrHappy <Justanimation007@gmail.com>
2024-09-05 23:09:35 +0200

    gravity change

--- scenes/Main_character.gd
+++ scenes/Main_character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -240.0
+const JUMP_VELOCITY = -200.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
