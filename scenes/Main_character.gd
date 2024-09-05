2a67812cd28aeed7debfe16e8d4ecb21b7e2f104
MrHappy <Justanimation007@gmail.com>
2024-09-05 23:01:40 +0200

    idk

--- scenes/Main_character.gd
+++ scenes/Main_character.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -200.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -240.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -36,7 +36,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-	var isleft = Input.is_action_just_pressed("left")
+	var isleft = velocity.x < 0
 	sprite_2d.flip_h = isleft
 	
 	if Input.is_action_just_pressed("left"):
