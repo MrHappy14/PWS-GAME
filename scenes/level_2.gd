af7a2eabbf63e98cd4c24872fac810ab311e92ad
MrHappy <Justanimation007@gmail.com>
2024-09-22 18:55:38 +0200

    Collectible gemaakt, level 1 design gemaakt, portal finish gemaakt en gekoppeld aan scripts en andere kleine dingetjes verfijnd in de code.

--- scenes/level_2.gd
+++ scenes/level_2.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
-func _ready():
-	var player = get_node("CharacterBody2D")  # Assuming "Player" is the name of your player node
-	if player:
-		player.set_gravity(75)  # Gravity specific to Level 2
-	var current_level = 2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
