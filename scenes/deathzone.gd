9a2792d49ddbf62a08307efeb5947a69845a2430
Piepmuis <163352878+Piepmuis1@users.noreply.github.com>
2024-09-24 19:50:16 +0200

    eindelijk

--- scenes/deathzone.gd
+++ scenes/deathzone.gd
@@ -1,8 +1,16 @@
-extends CollisionShape2D
+extends Area2D
 
-@export var target_level : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
-		get_tree().change_scene_to_packed(target_level)
-		queue_free()
+		get_tree().reload_current_scene()
