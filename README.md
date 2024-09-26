40ad6c1fc23540a9526d258ce47d05763b44cb73
Piepmuis <163352878+Piepmuis1@users.noreply.github.com>
2024-09-26 13:48:54 +0200

    kaas

--- README.md
+++
@@ -1,6 +0,0 @@
-# PWS GAME
-Greetings space enthousiast
-
-You are about to play the greatest space game that has ever existed.
-
-Good luck. You will need it.
--- scenes/pause.gd
+++ scenes/pause.gd
@@ -23,3 +23,7 @@ func _on_resume_pressed() -> void:
 func _on_main_menu_pressed() -> void:
 	get_tree().paused = false 
 	get_tree().change_scene_to_file("res://scenes/main menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
